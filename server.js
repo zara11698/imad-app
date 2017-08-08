ac30e07eaeeadd8eaf7d4378df41debb04695133
zara11698 <30663527+zara11698@users.noreply.github.com>
2017-08-08 17:47:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send('Certified bookworm alert!!!');
+  res.sendFile(path.join(__dirname, 'ui', 'bookworm.html'));
 });
 
 app.get('/article-two', function (req, res) {
